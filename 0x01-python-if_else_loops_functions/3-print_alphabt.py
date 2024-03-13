f2f5139d079436a833bc8798304ab49e52e6c730
Abayomi <olatunjiabayomi.o@gmail.com>
2024-03-13 05:01:02 +0100

    ptint alphabet

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+'''Using a loop to iterate through ASCII values\
+and printing the alphabet excluding 'q' and "e"'''
+for i in range(ord('a'), ord('z')+1):
+    if chr(i) not in ['q', 'e']:
+        print(chr(i), end='')
