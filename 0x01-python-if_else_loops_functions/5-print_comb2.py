b520dc37bef9cca4d813de529dfddf508e0af6d2
Abayomi <olatunjiabayomi.o@gmail.com>
2024-03-13 17:13:17 +0100

    update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i != 99:
-        print("{}, ".format(i), end="")
-    else:
-        print(f"{i}")
+  #  if i != 99:
+   #     print("{:02}, ".format(i), end="")
+   # else:
+    #    print(f"{i}")
+    print("{:02}".format(i), end=', ' if i < 99 else "\n")
