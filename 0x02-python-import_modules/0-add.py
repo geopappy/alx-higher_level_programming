67414e1702b6004fd0992d2f3ba3cef46607b157
Abayomi <olatunjiabayomi.o@gmail.com>
2024-03-14 18:22:26 +0100

    import and call function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,8 +2,12 @@
 from add_0 import add
 
 
-if __name__ == "__main__":
+def sum():
     a = 1
     b = 2
 
     print("{} + {} = {}".format(a, b, add(a, b)))
+
+
+if __name__ == "__main__":
+    sum()
