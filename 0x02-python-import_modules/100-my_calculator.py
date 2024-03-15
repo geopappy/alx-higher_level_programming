a8a5bdb002ecad23fb9fc68fd19ad1fa330797fd
Abayomi <olatunjiabayomi.o@gmail.com>
2024-03-15 04:09:18 +0100

    performs simple calculation

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,23 +6,24 @@ if __name__ == "__main__":
 
     import sys
 
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     opp = (sys.argv[2])
 
-    if sys.agrv != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
     if opp not in ['+', '-', '*', '/']:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
     match opp:
         case "+":
-            print("{} {} {} = {}".format(a, b, add(a, b)))
+            print("{} {} {} = {}".format(a, opp, b, add(a, b)))
         case "-":
-            print("{} {} {} = {}".format(a,       b, sub(a, b)))
+            print("{} {} {} = {}".format(a,       opp, b, sub(a, b)))
         case "*":
-            print("{} {} {} = {}".format(a,       b, mul(a, b)))
+            print("{} {} {} = {}".format(a,       opp, b, mul(a, b)))
         case "/":
-            print("{} {} {} = {}".format(a,       b, div(a, b)))
+            print("{} {} {} = {}".format(a,       opp, opp, div(a, b)))
