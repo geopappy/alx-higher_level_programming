c4eeb004fedc32662fc99be4a94ef5e2b2d18085
Abayomi <olatunjiabayomi.o@gmail.com>
2024-03-18 00:53:49 +0100

    print max number

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,8 +6,9 @@ def max_integer(my_list=[]):
 
     if len(my_list) == 0:
         return (0)
+    else:
 
-    for i in my_list:
-        if i > max:
-            max = i
-    return max
+        for i in my_list:
+            if i > max:
+                max = i
+        return max
