1af107803fdca3a9ef01e9c1f3b75750562db9b1
Abayomi <olatunjiabayomi.o@gmail.com>
2024-03-18 11:57:33 +0100

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,8 +4,7 @@ def no_c(my_string):
 
     new_str = ""
 
-    if len(my_string) > 0:
-        for i in my_string:
-            if i not in "cC":
-                new_str += i
-        return (new_str)
+    for i in my_string:
+        if i not in "cC":
+             new_str += i
+    return (new_str)
