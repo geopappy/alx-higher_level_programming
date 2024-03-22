13008dfd7cb29492f23bef5c10b68d6fb7e3c8ce
Abayomi <olatunjiabayomi.o@gmail.com>
2024-03-22 15:22:50 +0100

    uses map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_simple_map(matrix=[]):
-    # Define a function to square a single value
-    def square(x):
-        return x ** 2
-
-    squared_matrix = list(map(lambda row: list(map(square, row)), matrix))
-
-    return squared_matrix
+    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
