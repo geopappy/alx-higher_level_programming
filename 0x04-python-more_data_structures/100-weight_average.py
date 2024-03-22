bd309d98e06cb1ce716d4b5e8af6ac8788ee6e25
Abayomi <olatunjiabayomi.o@gmail.com>
2024-03-22 14:17:19 +0100

    calculate average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,6 +5,6 @@ def weight_average(my_list=[]):
     if not my_list:
         return 0
     for x in my_list:
-        upper += (x[0] + x[1])
+        upper += (x[0] * x[1])
         lower += x[1]
     return upper / lower
