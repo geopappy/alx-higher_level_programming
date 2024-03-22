49fbd76857d3250832f83d87f49094b688c179d1
Abayomi <olatunjiabayomi.o@gmail.com>
2024-03-22 13:13:16 +0100

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,12 +5,12 @@ def roman_to_int(roman_string):
         return 0
     roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':
                  1000}
-    roman_list = list(roman_string)
+    roman_list = list(roman_string.upper())
     prev_value, result = 0, 0
     for s in roman_list:
         if s not in roman_dic:
             return 0
-        value = roman_dic.get(s)
+        value = roman_dic[s]
         result += value
         if value > prev_value:
             result -= prev_value*2
