f8cc5b5322d373502abbafb1b71af823ad96d4e6
Abayomi <olatunjiabayomi.o@gmail.com>
2024-03-22 06:43:51 +0100

    covert roman yo int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+    roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':
+                 1000}
+    roman_list = list(roman_string.upper())
+    prev_value, result = 0, 0
+    for s in roman_list:
+        if s not in roman_dic:
+            return 0
+        value = roman_dic.get(s)
+        result += value
+        if value > prev_value:
+            result -= prev_value*2
+            prev_value = result
+    return result
