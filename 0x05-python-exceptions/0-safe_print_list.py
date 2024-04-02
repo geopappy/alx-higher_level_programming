f89aefd156f6185571a9c787ab0fcc9aa08eda9b
Abayomi <olatunjiabayomi.o@gmail.com>
2024-04-02 14:55:27 +0100

    raise indexError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         count = 0
         for i in range(x):
             print(my_list[i], end='')
-            count++
+            count += 1
     except IndexError:
         pass  # ignore index error for element beyond list len
     finally:
