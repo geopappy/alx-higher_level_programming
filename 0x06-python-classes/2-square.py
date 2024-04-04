20a751ad37c62ff4442044f008f709e7b9146680
Abayomi <olatunjiabayomi.o@gmail.com>
2024-04-04 15:18:59 +0100

    create Square class with size of defualt value= 0

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -21,7 +21,7 @@ class Square:
                 ValueError: If the size is less than 0.
         """
         if type(size) is not int:
-            raise TypeError("size must be integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >=0")
 
