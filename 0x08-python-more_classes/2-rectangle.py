4ffdbb3ffeef7807537e85417642b3aef819b7e9
Abayomi <olatunjiabayomi.o@gmail.com>
2024-04-08 23:47:26 +0100

    adds Area and Perimeter methods to Rectangle class

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -95,7 +95,7 @@ class Rectangle:
             int: The perimeter of a rectangle
 
         """
-        if width == 0 or height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
 
         return ((2 * self.__width) + (2 * self.__height))
