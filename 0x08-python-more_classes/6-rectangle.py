5ba68ea107724335f1d24805abd8e61af2a952ba
Abayomi <olatunjiabayomi.o@gmail.com>
2024-04-09 01:36:00 +0100

    add class classt attribute

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -121,4 +121,4 @@ class Rectangle:
     def __del__(self):
         """ Delete class instance"""
         print('Bye rectangle...')
-        number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
