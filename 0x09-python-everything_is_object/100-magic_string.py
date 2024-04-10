019e4a75ffa4ccbc97e4a18297b0d89b1e130c0b
Abayomi <olatunjiabayomi.o@gmail.com>
2024-04-10 06:31:21 +0100

    magic string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-"""prints magic string"""
 def magic_string():
     magic_string.n = getattr(magic_string, 'n', 0) + 1
     return "BestSchool" + ", BestSchool" * (magic_string.n - 1)
