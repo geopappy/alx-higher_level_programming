c348a6c977dc36d2710564c1c688f702b97ae101
Abayomi <olatunjiabayomi.o@gmail.com>
2024-04-12 02:20:38 +0100

    empty class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+""" module contains empty class"""
+
 
 class LockedClass:
+    """empty class"""
     __slots__ = ["first_name"]
 
     def __init__(self):
+        """ initialization"""
         pass
