d0bf6d2132eec7999f6ad351f9efde01bef468bd
Abayomi <olatunjiabayomi.o@gmail.com>
2024-04-12 02:15:47 +0100

    empty class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 class LockedClass:
-    def __setattr__(self, name, value):
-        if name != "first_name" and not hasattr(self, "first_name"):
-            raise AttributeError(f"'{type(self.__name__)}' object has no
-                                 attribute '{name}'")
-        else:
-            self.__dict__[name] = value
+    __slots__ = ["first_name"]
+
+    def __init__(self):
+        pass
