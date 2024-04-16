62b64c60925709eaea3da9c479ed96c8774189b7
Abayomi <olatunjiabayomi.o@gmail.com>
2024-04-16 01:15:30 +0100

    check for instance of an object

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Function tests if object is an instance of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ define the function """
+
+    return isinstance(obj, a_class)
