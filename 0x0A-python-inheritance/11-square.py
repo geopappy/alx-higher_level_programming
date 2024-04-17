bdf400a2902a645cc4500fdf705e3c4b67134a43
Abayomi <olatunjiabayomi.o@gmail.com>
2024-04-17 01:08:10 +0100

    add str method to square class

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -11,3 +11,11 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
+
+    def __str__(self):
+        """
+        prints a str rep for the rec.
+        Returns:
+            A string rep.
+        """
+        return f"[{self.__class__.__name__}] {self.__size}/{self.__size}"
