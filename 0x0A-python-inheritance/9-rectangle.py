f797a01390bf713b92c4653e390faa6d4417435d
Abayomi <olatunjiabayomi.o@gmail.com>
2024-04-17 01:58:23 +0100

    update

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -34,4 +34,4 @@ class Rectangle(BaseGeometry):
         Returns:
             A string rep.
         """
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        return f"[{self.__class__.__name__}] {self.__width}/{self.__height}"
