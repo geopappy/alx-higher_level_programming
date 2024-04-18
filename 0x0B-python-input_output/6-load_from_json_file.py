969bd4174f5b0b4fef90c7f4401c13d28e931624
Abayomi <olatunjiabayomi.o@gmail.com>
2024-04-18 01:59:48 +0100

    create Object from json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""import json"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    create object from a json file
+    """
+    with open(filename, 'r', encoding="utf-8") as file:
+        return json.loads(file)
