e5b19670163ad82d52d7991625e3379e58594229
Abayomi <olatunjiabayomi.o@gmail.com>
2024-04-18 01:24:50 +0100

    append to file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""1-write_file.py module"""
+
+
+def write_file(filename="", text=""):
+    """
+    write to a file.
+    Args:
+        filename: The file to write to.
+        text: content to write
+    Returns:
+        number of character written.
+    """
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        char_no = f.write(text)
+        return len(text)
