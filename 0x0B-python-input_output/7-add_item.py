780675f4272f61b3d34ba868300a5ba08ee280bb
Abayomi <olatunjiabayomi.o@gmail.com>
2024-04-19 01:25:22 +0100

    create and append file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""This module adds all arguments to a Python list and save them to a file."""
 
 import sys
 from os import path
+
 
 load = __import__('6-load_from_json_file').load_from_json_file
 save = __import__('5-save_to_json_file').save_to_json_file
