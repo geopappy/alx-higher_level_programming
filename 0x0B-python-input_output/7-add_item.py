becb83e34e602ca14c710eeda15f01d6ce6a4b06
Abayomi <olatunjiabayomi.o@gmail.com>
2024-04-19 01:09:01 +0100

    create and append file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+from os import path
+
+load = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
+"""
+try :
+    items = load('add_item.json')
+except Exception:
+    items = []
+items.extend(sys.argv[1:])
+save(items, 'add_item.json')"""
+
+filename = "add_item.json"
+
+if not path.exists(filename):
+    save([], filename)
+else:
+    items = load(filename)
+    items.extend(sys.argv[1:])
+    save(items, filename)
