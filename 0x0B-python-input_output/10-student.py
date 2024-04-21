e13ce30fb1062d6fcfaae5706ed43098b8e9b9a2
Abayomi <olatunjiabayomi.o@gmail.com>
2024-04-21 23:10:33 +0100

    defines student class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module defines student class"""
+
+
+class Student():
+    """Students class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            attrs = ['first_name', 'last_name', 'age']
+        return {attr: getattr(self, attr)  for attr in attrs}
